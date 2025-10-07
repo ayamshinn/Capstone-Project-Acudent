cc4cc2e9ca64ea35b1d74f289125eea65db5e629
AyamShinn <143488797+ayamshinn@users.noreply.github.com>
2025-10-08 00:02:20 +0800

    Add files via upload

---
+++ Acudent_backup-main/Javascript/patient-ui/patient-sidebar.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const patientSidebarContainer = document.getElementById('patient-sidebar-container');
+    const mainContentContainer = document.querySelector('.user-patient-content-area');
+    const topbar = document.getElementById('user-patient-topbar');
+    const toggleButton = document.querySelector('.toggle');
+
+    toggleButton.onclick = function() {
+        patientSidebarContainer.classList.toggle('active');
+        mainContentContainer.classList.toggle('active');
+        topbar.classList.toggle('active');
+    }
+
+    
+    
+});

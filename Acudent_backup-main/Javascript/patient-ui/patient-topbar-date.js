0adb98663d327c8678495d9380ab38f769dd0093
AyamShinn <143488797+ayamshinn@users.noreply.github.com>
2025-10-22 12:45:16 +0800

    Delete Acudent_backup-main directory

--- Acudent_backup-main/Javascript/patient-ui/patient-topbar-date.js
+++
@@ -1,7 +0,0 @@
-const dateElement = document.getElementById("today-date");
-
-const today = new Date();
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const formattedDate = today.toLocaleDateString('en-US', options);
-
-dateElement.textContent = formattedDate;
